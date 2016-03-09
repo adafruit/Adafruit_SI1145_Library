4608fa41d3ea629a8d057475f93718c4e7d9abdf
kent NX7H <fuzzballz@comcast.net>
2016-03-08 21:56:43 -0800

    Added and corrected comments

--- Adafruit_SI1145.cpp
+++ Adafruit_SI1145.cpp
@@ -23,10 +23,16 @@ Adafruit_SI1145::Adafruit_SI1145() {
 
 
 boolean Adafruit_SI1145::begin(void) {
+/* (default [I2C address of SI1145=0x60], [I2C bus number])
+   intitalizes to default mode (UV,Vis,IR and Prox 1)
+   enables all interupts and starts in autonomous mode*/
+
+  // Create I2C device
   Wire.begin();
  
   uint8_t id = read8(SI1145_REG_PARTID);
   if (id != 0x45) return false; // look for SI1145
+  
 #ifdef DEBUG
   dumpParam();
 #endif
@@ -59,27 +65,27 @@ boolean Adafruit_SI1145::begin(void) {
   writeParam(SI1145_PARAM_PS1ADCMUX, SI1145_PARAM_ADCMUX_LARGEIR);
   // prox sensor #1 uses LED #1
   writeParam(SI1145_PARAM_PSLED12SEL, SI1145_PARAM_PSLED12SEL_PS1LED1);
-  // fastest clocks, clock div 1
+  // fastest clocks, clock div 1 (integration time)
   writeParam(SI1145_PARAM_PSADCGAIN, 0);
-  // take 511 clocks to measure
+  // take 511 clocks to recover
   writeParam(SI1145_PARAM_PSADCOUNTER, SI1145_PARAM_ADCCOUNTER_511CLK);
   // in prox mode, high range
   writeParam(SI1145_PARAM_PSADCMISC, SI1145_PARAM_PSADCMISC_RANGE|
     SI1145_PARAM_PSADCMISC_PSMODE);
-
+  
+  /******************** IR Sensor ***************************/
   writeParam(SI1145_PARAM_ALSIRADCMUX, SI1145_PARAM_ADCMUX_SMALLIR);  
-  // fastest clocks, clock div 1
+  // fastest clocks, clock div 1 (integration time)
   writeParam(SI1145_PARAM_ALSIRADCGAIN, 0);
-  // take 511 clocks to measure
+  // take 511 clocks to recover
   writeParam(SI1145_PARAM_ALSIRADCOUNTER, SI1145_PARAM_ADCCOUNTER_511CLK);
   // in high range mode
   writeParam(SI1145_PARAM_ALSIRADCMISC, SI1145_PARAM_ALSIRADCMISC_RANGE);
 
-
-
-  // fastest clocks, clock div 1
+  /******************** Visible Sensor ***************************/
+  // fastest clocks, clock div 1 (integration time
   writeParam(SI1145_PARAM_ALSVISADCGAIN, 0);
-  // take 511 clocks to measure
+  // take 511 clocks to recover
   writeParam(SI1145_PARAM_ALSVISADCOUNTER, SI1145_PARAM_ADCCOUNTER_511CLK);
   // in high range mode (not normal signal)
   writeParam(SI1145_PARAM_ALSVISADCMISC, SI1145_PARAM_ALSVISADCMISC_VISRANGE);
@@ -100,8 +106,8 @@ void Adafruit_SI1145::reset() {
   write8(SI1145_REG_MEASRATE0, 0);
   write8(SI1145_REG_MEASRATE1, 0);
   write8(SI1145_REG_IRQEN, 0);
-  write8(SI1145_REG_IRQMODE1, 0);
-  write8(SI1145_REG_IRQMODE2, 0);
+  write8(SI1145_REG_IRQMODE1, 0); // not documented in data sheet
+  write8(SI1145_REG_IRQMODE2, 0); // not documented in data sheet
   write8(SI1145_REG_INTCFG, 0);
   write8(SI1145_REG_IRQSTAT, 0xFF);
 
@@ -150,6 +156,7 @@ uint16_t Adafruit_SI1145::readProx(void) {
 
 /*********************************************************************/
 
+// writes value to the parameter memory and verifies value is written 
 uint8_t Adafruit_SI1145::writeParam(uint8_t p, uint8_t v) {
   //Serial.print("Param 0x"); Serial.print(p, HEX);
   //Serial.print(" = 0x"); Serial.println(v, HEX);
@@ -159,6 +166,7 @@ uint8_t Adafruit_SI1145::writeParam(uint8_t p, uint8_t v) {
   return read8(SI1145_REG_PARAMRD);
 }
 
+// Reads (parameter) returns value of parameter, no decoding
 uint8_t Adafruit_SI1145::readParam(uint8_t p) {
   write8(SI1145_REG_COMMAND, p | SI1145_PARAM_QUERY);
   return read8(SI1145_REG_PARAMRD);
@@ -166,6 +174,7 @@ uint8_t Adafruit_SI1145::readParam(uint8_t p) {
 
 /*********************************************************************/
 
+// read unsign 8 bit value from register
 uint8_t  Adafruit_SI1145::read8(uint8_t reg) {
   uint16_t val;
     Wire.beginTransmission(_addr);
@@ -176,6 +185,7 @@ uint8_t  Adafruit_SI1145::read8(uint8_t reg) {
     return Wire.read();
 }
 
+// reads unsigned 16 bit value from consecutive registers starting with (register)
 uint16_t Adafruit_SI1145::read16(uint8_t a) {
   uint16_t ret;
 
@@ -190,6 +200,7 @@ uint16_t Adafruit_SI1145::read16(uint8_t a) {
   return ret;
 }
 
+//writes unsigned 8 bit val to register reg 
 void Adafruit_SI1145::write8(uint8_t reg, uint8_t val) {
 
   Wire.beginTransmission(_addr); // start transmission to device 
